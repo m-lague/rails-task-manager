15a302dd1aa2f502c1a08fd9b68197e554cb1d8f
Mathieu Lague <mathieu.lague@laposte.net>
2017-08-08 14:23:00 +0200

    add new_view edit_view _form Tasks#destroy Tasks#create Tasks#update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,26 +7,49 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find(params[:id])
+    if @task.updated_at == @task.created_at
+      @task.updated_at = ""
+    end
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
 #------------------------------------
   def done?
     @done
+  end
+
+  private
+
+  def task_params
+      params.require(:task).permit(:content)
   end
 
 end
